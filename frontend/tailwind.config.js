861b45407623022c9327afb2a9425203dcb231ea
danilo <danilo806@icloud.com>
2024-06-16 22:09:47 +0200

    feat: initial tailwind theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,11 +4,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#ff6363',
-        secondary: {
-          100: '#e2e2d5',
-          200: '#888883',
+        primary: '#7452BC',
+        secondary: '#FFDC56',
+        tertiary: '#554D62',
+        background: '#38364B',
+        'recipe-card-bg': '#FFFFFF',
+        text: {
+          primary: '#000000',
+          secondary: '#999999',
         },
+      },
+      borderRadius: {
+        'recipe-card': '1.5rem',
+        element: '9999px',
+      },
+      boxShadow: {
+        primary:
+          '0px 0px 15px 0px rgba(0,0,0,.03),0px 2px 30px 0px rgba(0,0,0,.08),0px 0px 1px 0px rgba(0,0,0,.3)',
       },
     },
   },
